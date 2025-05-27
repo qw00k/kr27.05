483985308d30d75e846764116e20b93f7cb90279
qw00k <antonkidyaev2006@icloud.com>
2025-05-27 10:32:48 +1000

    Add files via upload

---
+++ kr4n1.1.pas
@@ -0,0 +1,28 @@
+﻿unit pravilniimnogougolniki;
+
+interface
+
+function spravilnogotreugolnika(a: real): real;
+
+function skvadrata(a: real): real;
+
+function spravilnogopytiugolnika(a: real): real;
+
+implementation
+
+function spravilnogotreugolnika(a: real): real;
+begin
+  spravilnogotreugolnika := (sqrt(3) / 4) * a * a;
+end;
+
+function skvadrata(a: real): real;
+begin
+  skvadrata := a * a;
+end;
+
+function spravilnogopytiugolnika(a: real): real;
+begin
+  spravilnogopytiugolnika := (1/4) * sqrt(5 * (5 + 2 * sqrt(5))) * a * a;
+end;
+
+end.
