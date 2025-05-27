f9aee6c3ae5e87bf54368658248dc01cfe08b769
qw00k <antonkidyaev2006@icloud.com>
2025-05-27 10:34:07 +1000

    Add files via upload

---
+++ kr4n1.2.pas
@@ -0,0 +1,25 @@
+﻿program testpravilniimnogougolniki;
+
+uses
+  pravilniimnogougolniki;
+
+var
+  a: real;
+  n: integer;
+
+begin 
+  readln(a);
+  
+  writeln('1 правильный треугольник, 2 квадрат, 3 правильный пятиугольник');
+  readln(n);
+  
+  case n of
+    1: writeln(spravilnogotreugolnika(a):0:2);
+    2: writeln(skvadrata(a):0:2);
+    3: writeln(spravilnogopytiugolnika(a):0:2);
+  else
+    writeln(':(');
+  end;
+  
+  readln;
+end.
