5c26636bc3755049162c81532b780ad8f8034d2b
qw00k <antonkidyaev2006@icloud.com>
2025-05-27 11:04:38 +1000

    Add files via upload

---
+++ kr4n3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+void dvoichnoechislo(unsigned short chislo) {
+    for (int i = 0; i < 16; i++) {
+        cout << ((chislo >> i) & 1);
+    }
+}
+
+int main() {
+    unsigned short x;
+    cin >> x;
+
+    dvoichnoechislo(x);
+    cout << endl;
+
+    unsigned short resultat = x & 0b0101010101010101;
+
+    cout << resultat << endl;
+    dvoichnoechislo(resultat);
+    cout << endl;
+
+    return 0;
+}
